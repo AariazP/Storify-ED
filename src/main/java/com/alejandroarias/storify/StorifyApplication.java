16c3f5bd3d183ef9b0ed898eb2ada46ea7620909
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2023-03-17 19:55:51 -0500

    primer commit

---
+++ src/main/java/com/alejandroarias/storify/StorifyApplication.java
@@ -0,0 +1,13 @@
+package com.alejandroarias.storify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alejandroarias/storify/StorifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandroarias.storify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
